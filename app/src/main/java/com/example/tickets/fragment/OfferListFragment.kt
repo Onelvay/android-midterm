3b77c2f1bf73dfa83f7e6ac85fe65dec7eab53df
Onelvay <abajalen@gmail.com>
2024-10-26 15:59:19 +0500

    serizalizer fragment

--- app/src/main/java/com/example/tickets/fragment/OfferListFragment.kt
+++ app/src/main/java/com/example/tickets/fragment/OfferListFragment.kt
@@ -1,67 +1,71 @@
 package com.example.tickets.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tickets.R
 import com.example.tickets.adapter.OfferListAdapter
 import com.example.tickets.databinding.FragmentOfferListBinding
-import com.example.tickets.model.service.FakeService
-
+import com.example.tickets.viewmodel.OfferViewModel
+import kotlinx.coroutines.launch
 
 class OfferListFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = OfferListFragment()
-    }
-
     private var _binding: FragmentOfferListBinding? = null
-    private val binding
-        get() = _binding!!
-
-    private val adapter: OfferListAdapter by lazy {
-        OfferListAdapter()
-    }
+    private val binding get() = _binding!!
+    private val viewModel: OfferViewModel by viewModels()
+    private lateinit var adapter: OfferListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentOfferListBinding.inflate(layoutInflater, container, false)
-        return _binding?.root
+    ): View {
+        _binding = FragmentOfferListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        setupUI()
-
-        adapter.setItems(FakeService.offerList)
+        setupRecyclerView()
+        observeOffers()
+        setupSortOptions()
     }
 
-    private fun setupUI() {
-        with(binding) {
-            offerList.adapter = adapter
+    private fun setupRecyclerView() {
+        adapter = OfferListAdapter()
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = adapter
+    }
 
-            sortRadioGroup.setOnCheckedChangeListener { _, checkedId ->
-                when (checkedId) {
-                    R.id.sort_by_price -> {
-                        /**
-                         * implement sorting by price
-                         * hint: you can take the current list using getCurrentList method of ListAdapter instance
-                         */
-                    }
-
-                    R.id.sort_by_duration -> {
-                        /**
-                         * implement sorting by duration
-                         * hint: you can take the current list using getCurrentList method of ListAdapter instance
-                         */
-                    }
-                }
+    private fun observeOffers() {
+        lifecycleScope.launch {
+            viewModel.offers.collect { offers ->
+                adapter.submitList(offers)
             }
         }
+    }
+
+    private fun setupSortOptions() {
+        binding.radioButtonPrice.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                viewModel.sortOffersByPrice()
+            }
+        }
+
+        binding.radioButtonDuration.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                viewModel.sortOffersByDuration()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/tickets/model/entity/Airline.kt
+++ app/src/main/java/com/example/tickets/model/entity/Airline.kt
@@ -1,6 +1,8 @@
 package com.example.tickets.model.entity
 
+import com.google.gson.annotations.SerializedName
+
 data class Airline(
-    val name: String,
-    val code: String
+    @SerializedName("name") val name: String,
+    @SerializedName("code") val code: String
 )
